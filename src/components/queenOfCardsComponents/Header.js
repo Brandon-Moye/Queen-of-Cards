b2fb6a1bc4eff10250abf726081629bdd9abf339
Brandon Moye <brandon.m.moye@gmail.com>
2023-02-07 20:47:42 -0600

    updating css

--- src/components/queenOfCardsComponents/Header.js
+++ src/components/queenOfCardsComponents/Header.js
@@ -1,19 +1,37 @@
 import "../queenOfCardsComponents/Header.css";
+import React, { useState, useEffect } from "react";
+import { useAuth } from "../context/AuthContext";
+import { Link, useNavigate } from "react-router-dom";
+
 export default function Header() {
+  const [error, setError] = useState("");
+  const { currentUser, logout } = useAuth();
+  const navigate = useNavigate();
+
+  async function handleLogout() {
+    setError("");
+
+    try {
+      await logout();
+      navigate("/login");
+    } catch {
+      setError("Failed to log out");
+    }
+  }
   return (
     <div className="headerContainer">
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link
-        rel="preconnect"
-        href="https://fonts.gstatic.com"
-        crossOrigin="true"
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;600;700&display=swap"
-        rel="stylesheet"
-      />
-
       <h1 className="pageTitle">QUEEN OF CARDS</h1>
+      <div className="headerProfileInfoUpdateAndLogoutContainer">
+        {error}
+        <strong>{currentUser.email}</strong>
+
+        <Link to="/update-profile" className="updateProfileLink">
+          Update Profile
+        </Link>
+        <button className="logoutButton" type="link" onClick={handleLogout}>
+          Logout
+        </button>
+      </div>
       <a href="#" className="viewAllQueensButton">
         View All Queens
       </a>
